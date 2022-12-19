b60c98c5925eed78674287a9bb3093dff2be24c7
levworkshop <noreply@githubusers.com>
2022-12-19 18:45:10 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Form from './components/Form';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <div>
+                <NavLink
+                    to="/form"
+                >
+                    Form
+                </NavLink>
+            </div>
+
+
+            <Routes>
+                <Route
+                    path='/form'
+                    element={<Form />}
+                />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
