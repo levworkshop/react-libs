8bedaf3d12dc480b59648898e2a6f91ceacfe080
levworkshop <noreply@githubusers.com>
2022-12-19 19:43:38 +0200

    formik

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,52 @@
+import { useFormik } from "formik";
+
 function Form() {
+    const formik = useFormik({
+        // default values of fields
+        initialValues: {
+            firstName: '',
+            lastName: ''
+        },
+
+        onSubmit: values => {
+            // do something on submit...
+            console.log(values);
+        }
+    });
+
     return (
-        <>
-            Form
-        </>
+        <div>
+            <form
+                onSubmit={formik.handleSubmit}
+            >
+                <div>
+                    <label>First Name</label>
+                    <input
+                        type="text"
+                        name="firstName"
+                        id="firstName"
+                        value={formik.values.firstName}
+                        onChange={formik.handleChange}
+                    />
+                </div>
+                <div>
+                    <label>Last Name</label>
+                    <input
+                        type="text"
+                        name="lastName"
+                        id="lastName"
+                        value={formik.values.lastName}
+                        onChange={formik.handleChange}
+                    />
+                </div>
+
+                <button
+                    type="submit"
+                >
+                    Submit
+                </button>
+            </form>
+        </div>
     );
 }
 
