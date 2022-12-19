1103be07ab8eccfabfd93ed185b2f72f96e83f27
levworkshop <noreply@githubusers.com>
2022-12-19 21:09:39 +0200

    post request

--- src/components/HttpRequest.tsx
+++ src/components/HttpRequest.tsx
@@ -8,31 +8,53 @@ interface Post {
 
 function HttpRequest() {
 
-    const baseUrl = 'https://jsonplaceholder.typicode.com/users/1/';
+    const baseUrl = 'https://jsonplaceholder.typicode.com/';
     const [posts, setPosts] = useState<Array<Post>>([]);
 
     useEffect(() => {
         axios
-            .get(`${baseUrl}posts`)
+            .get(`${baseUrl}users/1/posts`)
             .then(response => {
                 setPosts(response.data);
             });
     }, []);
 
+    function createPost() {
+        axios
+            .post(`${baseUrl}posts`, {
+                title: 'new post title',
+                body: 'new post body'
+            })
+            .then(res => {
+                setPosts([
+                    ...posts,
+                    res.data
+                ]);
+            })
+    }
+
     if (posts.length === 0) return <div>No posts</div>;
 
     return (
-        <div>
-            {
-                posts.map(post =>
-                    <div
-                        key={post.id}
-                    >
-                        {post.title}
-                    </div>
-                )
-            }
-        </div>
+        <>
+            <div>
+                {
+                    posts.map(post =>
+                        <div
+                            key={post.id}
+                        >
+                            {post.title}
+                        </div>
+                    )
+                }
+            </div>
+
+            <button
+                onClick={createPost}
+            >
+                Post
+            </button>
+        </>
     );
 }
 
