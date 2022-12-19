ca3a3e10436c5369efe3b08446dd16758d6d822b
levworkshop <noreply@githubusers.com>
2022-12-19 20:11:01 +0200

    formik validate

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,32 @@
 import { useFormik } from "formik";
 
+interface IError {
+    firstName?: string;
+    lastName?: string;
+    city?: string;
+}
+
 function Form() {
     const formik = useFormik({
         // default values of fields
         initialValues: {
             firstName: '',
-            lastName: ''
+            lastName: '',
+            city: 'Tel-Aviv'
+        },
+
+        validate: values => {
+            const errors: IError = {};
+
+            if (values.firstName.length < 3) {
+                errors.firstName = "first name is too short";
+            }
+
+            if (values.lastName.length < 3) {
+                errors.lastName = "error: last name";
+            }
+
+            return errors;
         },
 
         onSubmit: values => {
@@ -27,7 +48,13 @@ function Form() {
                         id="firstName"
                         value={formik.values.firstName}
                         onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                     />
+                    {
+                        formik.touched.firstName && formik.errors.firstName ? (
+                            <div>{formik.errors.firstName}</div>
+                        ) : null
+                    }
                 </div>
                 <div>
                     <label>Last Name</label>
@@ -38,6 +65,23 @@ function Form() {
                         value={formik.values.lastName}
                         onChange={formik.handleChange}
                     />
+                    {
+                        formik.touched.lastName && formik.errors.lastName ? (
+                            <div>{formik.errors.lastName}</div>
+                        ) : null
+                    }
+                </div>
+                <div>
+                    <label>City</label>
+                    <select
+                        name="city"
+                        id="city"
+                        value={formik.values.city}
+                        onChange={formik.handleChange}
+                    >
+                        <option value="Tel-Aviv">Tel-Aviv</option>
+                        <option value="Haifa">Haifa</option>
+                    </select>
                 </div>
 
                 <button
